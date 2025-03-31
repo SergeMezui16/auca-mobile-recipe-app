967fdb29d03d36363f9af57aad549e2bffb31015
sergemezui <sergemezui.dev@gmail.com>
2025-04-01 00:56:01 +0100

    feat(app:drawer): integrate swipeable item for list actions
    Replaced custom swipe-to-delete implementation with `react-native-swipeable-item`. Improved list rendering efficiency and modularized UI with better swipeable item handling. Updated dependencies to include swipeable item support

--- app/(drawer)/swipe2.tsx
+++ app/(drawer)/swipe2.tsx
@@ -1,195 +1,61 @@
-import { Feather, Ionicons } from '@expo/vector-icons';
-import { useTheme } from '@react-navigation/native';
-import { useNavigation } from 'expo-router';
-import { useCallback, useLayoutEffect, useState } from 'react';
-import { Image, ScrollView, StyleSheet, View } from 'react-native';
-import Animated, {
-  FadeIn,
-  FadeInDown,
-  FadeOut,
-  LinearTransition,
-  SlideOutLeft,
-} from 'react-native-reanimated';
+import React, { useCallback } from 'react';
+import { FlatList, ListRenderItem, Text, TouchableOpacity, View } from 'react-native';
+import SwipeableItem, { useSwipeableItemParams } from 'react-native-swipeable-item';
 
-import { SlideToLeft } from '@/components/slide-to-left-1';
-import { Text } from '@/components/ui/text';
+const NUM_ITEMS = 10;
 
-export const SETTING_DATA = [
-  {
-    id: '1',
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-    profilePic: 'https://picsum.photos/id/11/200/300',
-  },
-  {
-    id: '2',
-    name: 'Jane Smith',
-    email: 'jane.smith@example.com',
-    profilePic: 'https://picsum.photos/id/26/200/300',
-  },
-  {
-    id: '3',
-    name: 'Michael Johnson',
-    email: 'michael.johnson@example.com',
-    profilePic: 'https://picsum.photos/id/43/200/300',
-  },
-  {
-    id: '4',
-    name: 'Emily Brown',
-    email: 'emily.brown@example.com',
-    profilePic: 'https://picsum.photos/id/94/200/300',
-  },
-  {
-    id: '5',
-    name: 'Daniel Wilson',
-    email: 'daniel.wilson@example.com',
-    profilePic: 'https://picsum.photos/id/57/200/300',
-  },
-];
-
-type LIST_DATA_ITEM = {
-  id: string;
-  name: string;
-  email: string;
-  profilePic: string;
-};
-type LIST_DATA = LIST_DATA_ITEM[];
-
-type RENDER_ITEM_TYPE = {
-  item: LIST_DATA_ITEM;
-  index: number;
-};
-
-export default function Swipe1() {
-  const navigation = useNavigation();
-  const theme = useTheme();
-  const [listData, setListData] = useState<LIST_DATA>(SETTING_DATA);
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight,
-    });
-  }, [navigation]);
-
-  const headerRight = useCallback(() => {
+export default function Swipe2() {
+  const renderItem: ListRenderItem<Item> = useCallback(({ item }) => {
     return (
-      <View style={{ backgroundColor: 'transparent' }}>
-        <Ionicons name="add" size={30} color={theme.colors.primary} />
-      </View>
+      <SwipeableItem
+        key={item.key}
+        item={item}
+        renderUnderlayLeft={() => <UnderlayLeft />}
+        renderUnderlayRight={() => <UnderlayLeft />}
+        snapPointsLeft={[150]}
+        snapPointsRight={[150]}>
+        <View className="h-10 w-full items-center justify-center bg-red-100 p-4">
+          <Text>{`${item.text}`}</Text>
+        </View>
+      </SwipeableItem>
     );
   }, []);
 
-  const deleteById = (id: string) => {
-    setListData((prev) => prev.filter((item) => item.id !== id));
-  };
-  const renderItem = (item: LIST_DATA_ITEM, index: number): JSX.Element => {
-    return (
-      <Animated.View
-        entering={FadeIn}
-        exiting={SlideOutLeft}
-        key={item.id}
-        layout={LinearTransition}>
-        {index !== 0 && <ItemSeparatorComponent />}
-        <SlideToLeft onRightPress={() => deleteById(item.id)}>
-          <View
-            style={[
-              styles.listItemContainer,
-              {
-                backgroundColor: theme.colors.card,
-              },
-            ]}>
-            <View
-              style={[
-                styles.imageContainer,
-                {
-                  backgroundColor: theme.colors.border,
-                },
-              ]}>
-              <Image source={{ uri: item.profilePic }} style={styles.imageStyle} />
-            </View>
-            <View style={styles.rightContainer}>
-              <Text
-                style={[
-                  styles.textStyle,
-                  {
-                    color: theme.colors.text,
-                  },
-                ]}>
-                {item.name}
-              </Text>
-              <Feather name="chevron-right" size={24} color={theme.colors.text} />
-            </View>
-          </View>
-        </SlideToLeft>
-      </Animated.View>
-    );
-  };
-
-  const ItemSeparatorComponent = () => {
-    return (
-      <View style={{ backgroundColor: theme.colors.card }}>
-        <View style={styles.separatorLine} />
-      </View>
-    );
-  };
   return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      <Animated.View layout={LinearTransition} style={styles.listContainer}>
-        {listData.map(renderItem)}
-      </Animated.View>
-
-      {listData?.length === 0 && (
-        <Animated.Text entering={FadeInDown} exiting={FadeOut} style={styles.noDataText}>
-          No items available. Click the '+' button to create a new one.
-        </Animated.Text>
-      )}
-    </ScrollView>
+    <View className="gap-4">
+      <FlatList keyExtractor={(item) => item.key} data={initialData} renderItem={renderItem} />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  listContainer: {
-    backgroundColor: 'rgba(100,100,100,0.1)',
-    marginHorizontal: 20,
-    borderRadius: 15,
-    marginTop: 10,
-    overflow: 'hidden',
-  },
-  separatorLine: {
-    backgroundColor: 'rgba(100,100,100,0.1)',
-    height: 2,
-    marginLeft: 74,
-  },
-  listItemContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  imageContainer: {
-    width: 50,
-    marginVertical: 10,
-    marginHorizontal: 12,
-    aspectRatio: 1,
-    borderRadius: 25,
-    overflow: 'hidden',
-  },
-  imageStyle: {
-    flex: 1,
-  },
-  rightContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginRight: 12,
-  },
-  textStyle: {
-    fontSize: 18,
-  },
-  noDataText: {
-    marginTop: 20,
-    fontSize: 18,
-    color: '#666666',
-    marginHorizontal: 30,
-    textAlign: 'center',
-  },
+const UnderlayLeft = () => {
+  const { close } = useSwipeableItemParams<Item>();
+  return (
+    <View>
+      <TouchableOpacity onPress={() => close()}>
+        <Text>CLOSE</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+type Item = {
+  key: string;
+  text: string;
+  backgroundColor: string;
+};
+
+function getColor(i: number) {
+  const multiplier = 255 / (NUM_ITEMS - 1);
+  const colorVal = i * multiplier;
+  return `rgb(${colorVal}, ${Math.abs(128 - colorVal)}, ${255 - colorVal})`;
+}
+
+const initialData: Item[] = [...Array(NUM_ITEMS)].fill(0).map((d, index) => {
+  const backgroundColor = getColor(index);
+  return {
+    text: `${index}`,
+    key: `key-${backgroundColor}`,
+    backgroundColor,
+  };
 });

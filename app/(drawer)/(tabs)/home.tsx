1a3de1cbcc273d1c692f9c3a15eb6b9b45ca69f6
sergemezui <sergemezui.dev@gmail.com>
2025-05-13 16:34:40 +0100

    refactor(home): update "Create my recipe" to use Link
    Replaced the "Create my recipe" button with a Link component for improved routing. Removed unused Shuffle button and its associated implementation for a cleaner UI and codebase.

--- app/(drawer)/(tabs)/home.tsx
+++ app/(drawer)/(tabs)/home.tsx
@@ -1,8 +1,9 @@
+import { Link } from 'expo-router';
 import React, { useState } from 'react';
 import { ScrollView, View } from 'react-native';
 
 import { SafeView } from '@/components/blocks';
-import { ArrowRightIcon, PlusIcon, ShuffleIcon } from '@/components/icons';
+import { ArrowRightIcon, PlusIcon } from '@/components/icons';
 import { RecipeList } from '@/components/recipes';
 import { Button } from '@/components/ui/button';
 import { Text } from '@/components/ui/text';
@@ -36,30 +37,16 @@ const Welcome = () => {
         Recipe Hub, create and share recipe with everybody around the world.
       </Text>
       <View className="flex flex-row gap-4">
-        <Button
-          onPress={async () => {
-            const res = await fetch('https://test.sergemezui.dev/', {
-              body: JSON.stringify({
-                identifier: 'UYTRERTYUIOP',
-                longitude: 23456789,
-                latitude: 23456789,
-                description: 'ERTYUIOP',
-                date: new Date().toISOString(),
-              }),
-              method: 'POST',
-              headers: { 'content-type': 'application/json' },
-            });
-
-            console.log(res.status);
-          }}
-          variant="outline">
-          <ShuffleIcon size={14} className="stroke-1 text-foreground" />
-          <Text>Shuffle recipe</Text>
-        </Button>
-        <Button>
-          <PlusIcon size={14} className="stroke-1 text-primary-foreground" />
-          <Text>Create my recipe</Text>
-        </Button>
+        {/*<Button variant="outline">*/}
+        {/*  <ShuffleIcon size={14} className="stroke-1 text-foreground" />*/}
+        {/*  <Text>Shuffle recipe</Text>*/}
+        {/*</Button>*/}
+        <Link asChild href={{ pathname: '/new' }}>
+          <Button>
+            <PlusIcon size={14} className="stroke-1 text-primary-foreground" />
+            <Text>Create my recipe</Text>
+          </Button>
+        </Link>
       </View>
     </SafeView>
   );

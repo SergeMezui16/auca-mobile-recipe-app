7c085ccb136ef1c00f977579a4718857d86b1c91
sergemezui <sergemezui.dev@gmail.com>
2025-05-13 16:35:07 +0100

    feat(search): add live search with filtered recipes list
    Implemented a live search feature that filters recipes based on user input. Replaced `RecipeList` with `FlashList` for optimized rendering of the filtered recipe cards using `useLiveQuery` and `useRecipe` hooks. Improved user experience by making the search dynamic and responsive.
    ```

--- app/(drawer)/(tabs)/search.tsx
+++ app/(drawer)/(tabs)/search.tsx
@@ -1,17 +1,31 @@
+import { FlashList } from '@shopify/flash-list';
+import { useLiveQuery } from 'drizzle-orm/expo-sqlite';
+import React, { useState } from 'react';
 import { SafeAreaView, ScrollView, View } from 'react-native';
 
-import { RecipeList } from '@/components/recipes';
+import { RecipeCard } from '@/components/recipes';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Text } from '@/components/ui/text';
+import { useRecipe } from '@/hooks/use-recipe';
 
 export default function Search() {
+  const [search, setSearch] = useState('');
+  const now = new Date();
+  const { getAllRecipePublished } = useRecipe();
+  const { data: recipes } = useLiveQuery(getAllRecipePublished(), [now]);
+  const filteredRecipes = recipes?.filter((recipe) =>
+    recipe.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <View className="flex-1">
       <SafeAreaView className="m-2 flex-row gap-2">
         <Input
           placeholder="search a recipe..."
           id="search"
+          value={search}
+          onChangeText={(v) => setSearch(v)}
           className="flex-1 font-[rosarivo]"
           aria-labelledby="inputLabel"
           aria-errormessage="inputError"
@@ -21,7 +35,12 @@ export default function Search() {
         </Button>
       </SafeAreaView>
       <ScrollView className="m-2">
-        <RecipeList />
+        <FlashList
+          data={filteredRecipes}
+          ItemSeparatorComponent={() => <View className="h-6" />}
+          renderItem={({ item }) => <RecipeCard recipe={item} />}
+          estimatedItemSize={153}
+        />
       </ScrollView>
     </View>
   );
